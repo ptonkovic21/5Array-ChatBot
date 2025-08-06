1678154140ec864fa693a62941ae81a5ddb9fa53
Petar Tonković <ptonkovic21@student.foi.hr>
2025-08-06 16:57:45 +0200

    Minor format changes

--- Software/deepseek_test.py
+++ Software/deepseek_test.py
@@ -5,10 +5,14 @@ model_name = "deepseek-ai/deepseek-llm-7b-chat"
 
 tokenizer = AutoTokenizer.from_pretrained(model_name)
 
+embeddings = HuggingFaceEmbeddings()
+
 model = AutoModelForCausalLM.from_pretrained(
     model_name, 
     torch_dtype=torch.bfloat16, 
     device_map="auto")
+
+
 model.generation_config = GenerationConfig.from_pretrained(model_name)
 
 model.generation_config.pad_token_id = model.generation_config.eos_token_id
