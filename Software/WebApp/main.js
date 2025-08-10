b8a803d1e0269a8f6bbe244ff34fbcbcffaed10e
Petar Tonković <ptonkovic21@student.foi.hr>
2025-08-10 22:13:24 +0200

    Created index.html and sytle.css for the fronted of the app, main.js updated to support multiple requests

--- Software/WebApp/main.js
+++ Software/WebApp/main.js
@@ -1,12 +1,39 @@
-async function askQuestion(question) {
-	const response = await fetch("http://localhost:8080/ask", {
-		method: "POST",
-		headers: { "Content-Type": "application/json" },
-		body: JSON.stringify({ question }),
+window.addEventListener("load", () => {
+	send_button = document.getElementById("send_button");
+
+	send_button.addEventListener("click", () => {
+		question_element = document.getElementById("question");
+		question = question_element.value;
+
+		question_element = "";
+
+		chatBox.innerHTML += `<span class="you">You:</span> ${question}<br>`;
+
+		ask(question);
 	});
+});
 
-	const data = await response.json();
-	console.log("5Array: ", data.answer);
+async function ask(question) {
+	if (!question) return;
+
+	let chatBox = document.getElementById("chat");
+
+	thinking =
+		chatBox.innerHTML += `<span class="bot">5Array is thinking...</span><br>`;
+
+	try {
+		const res = await fetch("http://localhost:8080/ask", {
+			method: "POST",
+			headers: { "Content-Type": "application/json" },
+			body: JSON.stringify({ question }),
+		});
+		const data = await res.json();
+
+		chatBox.innerHTML = chatBox.innerHTML.replace(
+			thinking,
+			`<span class="bot">5Array:</span> ${data.answer}<br>`
+		);
+	} catch (err) {
+		chatBox.innerHTML += `<span class="bot">Error:</span> ${err.message}<br>`;
+	}
 }
-
-askQuestion("Who are you?");
